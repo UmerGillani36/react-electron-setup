95753c569bd0d1a79e19a806205b7778943835dc
sufiyan <sufiyan2401>
2023-07-26 18:08:12 +0500

    app

--- react-crud-app/src/App.js
+++ react-crud-app/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+const App = () => {
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  console.log("data", data);
+  const fetchData = () => {
+    fetch("http://localhost:9000/api/getData")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error));
+  };
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+  const handleSubmit = (event) => {
+    fetch("http://localhost:9000/api/insertData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: name }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Inserted record with ID:", data.id);
+        // Fetch updated data after successful insertion
+        fetchData();
+      })
+      .catch((error) => console.error(error));
 
-function App() {
+    // Reset the name input field
+    setName("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>SQLite3 Data</h1>
+      <input
+        type="text"
+        placeholder="Enter the name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{ marginRight: "20px" }}
+      />
+      <button onClick={handleSubmit}>Insert Data</button>
+      <ul>
+        {data.length > 1 &&
+          data?.map((item) => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
