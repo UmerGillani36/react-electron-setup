e111e11a90692c48cf1b3c66139cf5262df61096
sufiyan <sufiyan2401>
2023-08-07 12:46:08 +0500

    First-Commit

---
+++ purity-ui-dashboard-main/src/views/Dashboard/Tables/components/Authors.js
@@ -0,0 +1,32 @@
+// Chakra imports
+import {
+  Table,
+  Tbody,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useColorModeValue,
+} from "@chakra-ui/react";
+// Custom components
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
+import CardHeader from "components/Card/CardHeader.js";
+import TablesTableRow from "components/Tables/TablesTableRow";
+import React from "react";
+
+const Authors = ({ title, captions, data }) => {
+  const textColor = useColorModeValue("gray.700", "white");
+  return (
+    <Card overflowX={{ sm: "scroll", xl: "hidden" }}>
+      <CardHeader p="6px 0px 22px 0px">
+        <Text fontSize="xl" color={textColor} fontWeight="bold">
+          {/* {title} */}
+        </Text>
+      </CardHeader>
+      <CardBody></CardBody>
+    </Card>
+  );
+};
+
+export default Authors;
